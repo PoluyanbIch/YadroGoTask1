a1cd81096d10d9c83db2d9291513f2688275d515
PoluyanbIch <poluyanovia@yandex.ru>
2025-10-16 22:01:08 +0300

    hello/hello release

--- hello/server.go
+++ hello/server.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Server is listening on port 8080...")
 	http.ListenAndServe(":8080", nil)
@@ -19,4 +20,14 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("pong"))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	name := r.URL.Query().Get("name")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
 }
