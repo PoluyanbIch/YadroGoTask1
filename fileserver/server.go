8e0ebc87f7086a5cf1b30af02f69bdb7052945a6
PoluyanbIch <poluyanovia@yandex.ru>
2025-10-20 15:28:12 +0300

    fileserver/files GET release, fileserver/files/filename PUT release

--- fileserver/server.go
+++ fileserver/server.go
@@ -6,23 +6,29 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const dir = "./upload"
 
 func main() {
 	http.HandleFunc("/files", filesHandler)
+	http.HandleFunc("/files/", filenameHandler)
 
 	fmt.Println("Server is listening on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
 
 func filesHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	switch r.Method {
+	case http.MethodPost:
+		filesHandlerPost(w, r)
+	case http.MethodGet:
+		filesHandlerGet(w, r)
+	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	filesHandlerPost(w, r)
 }
 
 func filesHandlerPost(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +46,7 @@ func filesHandlerPost(w http.ResponseWriter, r *http.Request) {
 
 	err = os.MkdirAll(dir, 0755)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error in male dir: %s", err.Error()), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Error in making dir: %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
 
@@ -67,4 +73,73 @@ func filesHandlerPost(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("File upploading success"))
+}
+
+func filesHandlerGet(w http.ResponseWriter, r *http.Request) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Reading Directory Error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	for _, file := range files {
+		w.Write([]byte(file.Name() + "\n"))
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func filenameHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	filename := strings.TrimPrefix(path, "/files/")
+
+	if filename == "" {
+		http.Error(w, "Filename required", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodPut:
+		filenameHandlerPut(w, r, filename)
+	case http.MethodGet:
+		return
+	case http.MethodDelete:
+		return
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func filenameHandlerPut(w http.ResponseWriter, r *http.Request, filename string) {
+	filePath := filepath.Join(dir, filename)
+
+	if _, err := os.Stat(filePath); err != nil {
+		http.Error(w, fmt.Sprintf("File not found Error: %s", err.Error()), http.StatusNotFound)
+		return
+	}
+
+	prevFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Open file Error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	defer prevFile.Close()
+
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, fmt.Sprintf("ParseMultipleForm Error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("FormFile Error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(prevFile, file); err != nil {
+		http.Error(w, fmt.Sprintf("Copying Error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
